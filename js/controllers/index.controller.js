999aad3fdf2818cd2a4d3563bae8799e221b14b7
Fr4nkVill4nuev4 <20250827@ricaldone.edu.sv>
2026-06-19 11:19:24 -0600

    first commit

---
+++ js/controllers/index.controller.js
@@ -0,0 +1,90 @@
+let tareas = [];
+
+const txtTarea = document.getElementById("txtTarea");
+const prioridad = document.getElementById("prioridad");
+const btnAgregar = document.getElementById("btnAgregar");
+const listaTareas = document.getElementById("listaTareas");
+const contador = document.getElementById("contador");
+
+btnAgregar.addEventListener("click", agregarTarea);
+
+function agregarTarea() {
+    // CORREGIDO: Se cambió 'txtTarea.Tarea.value' por 'txtTarea.value'
+    const nombreTarea = txtTarea.value.trim();
+    const prioridadTarea = prioridad.value;
+
+    if (nombreTarea === "" || prioridadTarea === "") {
+        Swal.fire({
+            icon: "warning",
+            title: "Campos incompletos",
+            text: "Debes escribir una tarea y seleccionar una prioridad.",
+            confirmButtonText: 'Aceptar'
+        });
+        return;
+    }
+
+    const nuevaTarea = {
+        nombre: nombreTarea,
+        prioridad: prioridadTarea,
+    };
+
+    // CORREGIDO: Se cambió 'tarea.push' por 'tareas.push' para usar el arreglo global
+    tareas.push(nuevaTarea);
+
+    mostrarTareas();
+    limpiarFormulario();
+
+    Swal.fire({
+        icon: "success",
+        title: "Tarea agregada",
+        text: "La tarea se agregó correctamente.",
+        confirmButtonText: "Aceptar"
+    });
+}
+
+function mostrarTareas() {
+    listaTareas.innerHTML = "";
+
+    tareas.forEach(function (tarea, index) {
+        
+        listaTareas.innerHTML += `
+        <article class="bg-light rounded-3 p-3 d-flex justify-content-between align-items-center mb-2">
+                <div>
+                    <h5 class="mb-1">${tarea.nombre}</h5>
+                    <p class="mb-0 text-muted">${tarea.prioridad}</p>
+                </div>
+
+                <div>
+                    <button class="btn btn-danger btn-sm" onclick="eliminarTarea(${index})">
+                        Eliminar
+                    </button>
+                </div>
+            </article>
+        `;
+    });
+
+    contadorDeTareas();
+}
+
+function eliminarTarea(index) {
+    tareas.splice(index, 1);
+
+    mostrarTareas();
+
+    Swal.fire({
+        icon: "success",
+        title: "Tarea eliminada",
+        text: "La tarea se eliminó correctamente.",
+        confirmButtonText: "Aceptar"
+    });
+}
+
+function limpiarFormulario() {
+    txtTarea.value = "";
+    prioridad.value = "";
+    txtTarea.focus();
+}
+
+function contadorDeTareas() {
+    contador.innerText = tareas.length;
+}
